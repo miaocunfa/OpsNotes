3476a8e95407016753d34ff96e5659edbf8f899f
miao <miaocunf@163.com>
2020-03-16 12:38:32 +0800

    add export_jdxt_sql.sh

---
+++ Script/export_jdxt_sql.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+curDate=`date +'%Y%m%d'`
+
+cd /home/miaocunfa
+
+echo
+echo "exporting..."
+mysqldump -uroot -B jdxt_db > jdxt_db_${curDate}.sql
+echo "export successfully!"
+echo "SQL File: "
+ls -lh /home/miaocunfa/jdxt_db_${curDate}.sql
+
+echo
+echo "Taring..."
+tar -zcvf jdxt_db_${curDate}.tgz ./jdxt_db_${curDate}.sql
+echo "Tar successfully!"
+
+echo
+echo "remove SQL File!"
+rm jdxt_db_${curDate}.sql
+
+echo
+echo "Tar File: "
+ls -lh /home/miaocunfa/jdxt_db_${curDate}.tgz
+echo
